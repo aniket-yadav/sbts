43c1b73e7f400732d6701e6321750967706deca2
Aniket Yadav <aniket.y@cruxbytes.com>
2022-02-20 14:41:05 +0530

    change password

--- app/src/main/java/com/app/sbts/activities/ParentActivity.java
+++ app/src/main/java/com/app/sbts/activities/ParentActivity.java
@@ -1,30 +1,61 @@
 package com.app.sbts.activities;
-
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.app.sbts.R;
+import com.app.sbts.classes.SessionManager;
+import com.app.sbts.classes.SingletonClass;
 import com.app.sbts.databinding.ActivityParentBinding;
+import com.app.sbts.databinding.NavHeaderMainBinding;
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class ParentActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityParentBinding binding;
+    SharedPreferences sharedPreferences;
+    SessionManager sessionManager;
+    SharedPreferences.Editor editor;
+    Bitmap bitmap;
+    StringRequest stringRequest;
+    String[] str;
+    private  NavHeaderMainBinding headerBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityParentBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setSupportActionBar(binding.appBarMain.toolbar);
+        sharedPreferences = getSharedPreferences("LOGIN", Context.MODE_PRIVATE);
 
+        sessionManager = new SessionManager(this);
+        HashMap<String, String> user = sessionManager.getUserDetails();
 
+        View headerView = binding.navView.getHeaderView(0);
+        headerBinding = NavHeaderMainBinding.bind(headerView);
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -38,9 +69,11 @@ public class ParentActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
         navigationView.getMenu().findItem(R.id.nav_logout_parent).setOnMenuItemClickListener(menuItem -> {
-            Log.i("navController", String.valueOf(menuItem.getTitle()));
+            sessionManager.logout();
             return true;
         });
+
+        getData();
     }
 
     @Override
@@ -52,6 +85,51 @@ public class ParentActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
+    private void getData() {
+
+        ;
+        stringRequest = new StringRequest(Request.Method.POST,  getString(R.string.Parent_URL),
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        editor = sharedPreferences.edit();
+
+                        str = Pattern.compile(",").split(response);
+                        editor.putString("Full_Name", str[0]);
+                        editor.putString("Photo", str[1]);
+                        editor.putString("Email", str[2]);
+                        editor.putString("Mobile_No1", str[3]);
+                        editor.putString("Bus_No", str[4]);
+                        editor.putString("DOB", str[5]);
+                        editor.putString("Student_Name", str[6]);
+                        editor.putString("Address", str[7]);
+                        editor.putString("Latitude",str[8]);
+                        editor.putString("Longitude",str[9]);
+                        editor.apply();
+
+                        String Photo = sharedPreferences.getString("Photo", null);
+                        byte[] imagebit = Base64.decode(Photo, Base64.DEFAULT);
+                        headerBinding.imageView.setImageBitmap(BitmapFactory.decodeByteArray(imagebit, 0, imagebit.length));
+
+                        headerBinding.userName.setText(sharedPreferences.getString("Full_Name", null));
+                        headerBinding.userEmail.setText(sharedPreferences.getString("Email", null));
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("username", Objects.requireNonNull(sharedPreferences.getString("USERNAME", "NULL")));
+                return params;
+            }
+        };
+        SingletonClass.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
+    }
 
 
     @Override
--- app/src/main/java/com/app/sbts/fragments/ChangePassword.java
+++ app/src/main/java/com/app/sbts/fragments/ChangePassword.java
@@ -3,13 +3,24 @@ package com.app.sbts.fragments;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
+import com.app.sbts.R;
+import com.app.sbts.classes.SessionManager;
+import com.app.sbts.classes.SingletonClass;
 import com.app.sbts.databinding.FragmentChangePasswordBinding;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class ChangePassword extends Fragment {
 
@@ -20,6 +31,42 @@ public class ChangePassword extends Fragment {
                              Bundle savedInstanceState) {
         binding =  FragmentChangePasswordBinding.inflate(inflater, container, false);
         return  binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SessionManager sessionManager = new SessionManager(requireActivity().getApplicationContext());
+        final HashMap<String, String> user = sessionManager.getUserDetails();
+       binding.passwordChange.setOnClickListener(v -> {
+           if (Objects.requireNonNull(binding.oldPassword.getText()).toString().isEmpty()) {
+               Toast.makeText(requireActivity().getApplicationContext(), getString(R.string.old_pass_error), Toast.LENGTH_LONG).show();
+
+           } else if (Objects.requireNonNull(binding.newPassword.getText()).toString().isEmpty()) {
+               Toast.makeText(requireActivity().getApplicationContext(),getString( R.string.new_password_error), Toast.LENGTH_LONG).show();
+
+           } else if (binding.oldPassword.getText().toString().equalsIgnoreCase(binding.newPassword.getText().toString())) {
+
+               Toast.makeText(requireActivity().getApplicationContext(), getString(R.string.same_pass_error), Toast.LENGTH_LONG).show();
+
+           } else {
+
+               StringRequest stringRequest = new StringRequest(Request.Method.POST, getString(R.string.Change_Password_URL), response -> Toast.makeText(requireActivity().getApplicationContext(), response, Toast.LENGTH_LONG).show(), error -> Toast.makeText(requireActivity().getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show()) {
+                   @Override
+                   protected Map<String, String> getParams() {
+                       Map<String, String> params = new HashMap<>();
+                       params.put("username", user.get(SessionManager.USERNAME));
+                       params.put("role",user.get(SessionManager.ROLE));
+                       params.put("old_password", binding.oldPassword.getText().toString());
+                       params.put("new_password", binding.newPassword.getText().toString());
+                       return params;
+                   }
+               };
+               SingletonClass.getInstance(requireActivity().getApplicationContext()).addToRequestQueue(stringRequest);
+
+           }
+       });
+
     }
 
     @Override
